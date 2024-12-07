49b4d5bc96037ec2308996ca25c3b1ad3e0ee651
GaahNoel <ga.noel@outlook.com>
2024-12-06 23:26:32 -0300

    chore: add and configure vitest

---
+++ vitest.config.ts
@@ -0,0 +1,27 @@
+import path from 'path'
+import { configDefaults, defineConfig } from 'vitest/config'
+
+export default defineConfig({
+
+  test: {
+    coverage: {
+      all: true,
+      include: ['src/**/*.ts'],
+      exclude: [...configDefaults.exclude, 'prisma/**'],
+      provider: 'v8',
+      thresholds: {
+        statements: 100,
+        branches: 100,
+        functions: 100,
+        lines: 100,
+      },
+    },
+    silent: true,
+  },
+  resolve: {
+    alias: {
+      '@/tests': path.resolve(__dirname, './tests'),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
